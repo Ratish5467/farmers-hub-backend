68c1bea530a56cea35e90399c05207e81cbc704e
Ratish Kumar <163174206+Ratish5467@users.noreply.github.com>
2025-09-11 12:27:52 +0530

    Update app.js

--- utils/app.js
+++ utils/app.js
@@ -5,7 +5,7 @@ const app=express();
 
 
 app.use(cors({
-  origin: 'https://farmers-hub-frontend-code.vercel.app',
+  origin: '*',
   methods: ['GET','POST','PUT','DELETE'],
   credentials: true
 }));

bea34b41fd1640a2bfeb1a6f768071733ae71b25
Ratish Kumar <163174206+Ratish5467@users.noreply.github.com>
2025-11-05 10:48:38 +0530

    Restrict CORS origin to specific frontend URL

--- utils/app.js
+++ utils/app.js
@@ -5,7 +5,7 @@ const app=express();
 
 
 app.use(cors({
-  origin: '*',
+  origin: 'https://farmers-hub-frontend-code.vercel.app/',
   methods: ['GET','POST','PUT','DELETE'],
   credentials: true
 }));
